ec61f2b6067a3e069ce521b657565ab284e7c61b
rafael <rafaelbercosta@gmail.com>
2022-07-11 18:44:28 -0300

    Primeiro envio requisito 1 bora bora

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,7 +2,22 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
-describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('1 - Test fetchProducts function', () => {
+  it('should be a function', () => {
+    expect(typeof fetchProducts).toEqual('function');
+  });
+  it('should call fetch', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalled()
+  });
+  it('should request the correct endpoint', async () =>{
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it('should have the same data structure of the object', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+  it('should return Error without an argument', async() => {
+    expect(await fetchProducts()).toEqual(new Error('You must provide an url'));
+  });
 });
