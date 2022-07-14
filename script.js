988b50a6b56448a940555adc833c7dff33cca6e9
rafael <rafaelbercosta@gmail.com>
2022-07-13 21:39:50 -0300

    Cumprido  o requisito 5

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const createProductItemElement = ({ sku, name, image }) => {
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  // coloque seu código aqui
+  event.target.remove();
 };
 
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
